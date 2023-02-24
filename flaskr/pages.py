384292f7400ddc5435ee2d5687a75b24421447c7
Chelsea Garcia <che.garc@techexchange.in>
2023-02-24 21:22:49 +0000

    Foundations of home and pages

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask import Flask
 
 
 def make_endpoints(app):
@@ -9,6 +10,22 @@ def make_endpoints(app):
     def home():
         # TODO(Checkpoint Requirement 2 of 3): Change this to use render_template
         # to render main.html on the home page.
+        return render_template("main.html")
+
+    @app.route("/pages")
+    def pages():
+        return render_template("pages.html")
+
+    @app.route("/pages/redHotChiliPeppers")
+    def redHotChiliPeppers():
+        return render_template("main.html")
+
+    @app.route("/pages/nirvana")
+    def nirvana():
+        return render_template("main.html")
+    
+    @app.route("/pages/deftones")
+    def deftones():
         return render_template("main.html")
 
     # TODO(Project 1): Implement additional routes according to the project requirements.
