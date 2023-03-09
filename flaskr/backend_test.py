262f91f539c7c25f1ce14626720cf7242b7db9b9
Sebastian <sebastiancaballero@techexchange.in>
2023-03-09 00:13:25 +0000

    Finished get_image test

--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -1,5 +1,6 @@
 from flaskr.backend import Backend
 from unittest.mock import MagicMock
+import base64
 
 
 def test_get_all_page_names():
@@ -13,18 +14,23 @@ def test_get_all_page_names():
 def test_get_image():
     storage_client = MagicMock()
     image_bucket = MagicMock()
-    backend = Backend(storage_client)
 
     storage_client.bucket.return_value = image_bucket
 
     blob = MagicMock()
     image_bucket.get_blob.return_value = blob
 
+    bytes_val = base64.b64encode(b"Test").decode("utf-8")
     blob.download_as_bytes.return_value = b"Test"
 
-    blob.content_type.return_value = "image/jpeg"
+    blob.content_type = "image/jpeg"
 
-    test = backend.get_image("test.jpeg", "about")
-    print(test)
+    backend = Backend(storage_client)
+    cont_type, img = backend.get_image("test.jpeg", "about")
+
+    bytes_val = base64.b64encode(b"Test").decode("utf-8")
+    
+    assert img == bytes_val
+    
 
 
