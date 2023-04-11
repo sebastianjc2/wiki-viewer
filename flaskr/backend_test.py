8187e03318cf8c06393eb49ff657f6663d1c5dc2
Sebastian <sebastiancaballero@techexchange.in>
2023-04-11 00:02:03 +0000

    Added test to see if get_user_info was working as intended

--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -208,3 +208,18 @@ def test_sign_in_password_pass():
     )
     #Asserts 'Passed' for successfully signing in using the correct password
     assert mocker.sign_in('test', 'testpass') == 'Passed'
+
+
+def test_get_user_info():
+    ''' Tests the get_user_info function by making sure that it returns the expected info
+    which means it correctly collected the user's information. '''
+    # Mocking the storage client, the backend, the bucket and the blobs.
+    storage_client = MagicMock()
+    mocker = Backend(storage_client)
+    test_user_info = storage_client.bucket.return_value
+    test_blob = test_user_info.blob.return_value
+
+    # using mock open to mock the user.open("r") as f command
+    test_blob.open = mock_open('{"test1":"test", "test2":"test"}')
+
+    assert mocker.get_user_info("test") == {"test1": "test", "test2": "test"}
